e6414013aed49843a0700b2b012eaab92104b76f
darshan <dsp2293@gmail.com>
2022-01-17 11:11:54 -0500

    First Commite

---
+++ src/test/java/qaclickacademy/Appiumtest.java
@@ -0,0 +1,45 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Appiumtest {
+
+	
+	
+	@Test
+	public void NativeAPPAndroid() {
+		
+		System.out.println("NativeAPPAndroid");
+		
+	}
+	
+	
+	
+	@Test
+	public void IosAPP()
+	{
+		
+		
+		
+		System.out.println("IosAPP");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/qaclickacademy/Seleniumtest.java
@@ -0,0 +1,34 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Seleniumtest {
+
+	
+	
+	@Test
+	
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+		
+	}
+	
+	
+	
+	@Test
+	public void ElementUI()
+	{
+		
+		System.out.println("ElementUI");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
